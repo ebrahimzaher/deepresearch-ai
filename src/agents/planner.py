0c7551fb59da0fe8799ce61b52ed5b59b611947c
Ebrahim Zaher <ebrahimzaherr0@gmail.com>
2026-06-02 22:09:36 +0300

    refactor: update planner agent to accept and process context summary

--- src/agents/planner.py
+++ src/agents/planner.py
@@ -1,37 +1,40 @@
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
-from src.prompts.planner_prompt import PLANNER_PROMPT
+from src.prompts import PLANNER_PROMPT
 import json
 
 load_dotenv()
 
 llm = ChatGroq(
-model="openai/gpt-oss-120b",
-temperature=0
+    model="openai/gpt-oss-120b",
+    temperature=0
 )
 
-prompt = ChatPromptTemplate.from_messages([
-("system", PLANNER_PROMPT),
-("human", "{query}")
-])
+prompt = ChatPromptTemplate.from_messages([("system", PLANNER_PROMPT)])
 
 planner_chain = prompt | llm
 
-def planner_agent(query: str):
+def planner_agent(query: str, context_summary: str = None):
+    if not context_summary:
+        context_summary = "No previous context."
+
+    print("\n===== SUMMARY SENT TO PLANNER =====")
+    print(context_summary)
+    print("===================================\n")
 
     response = planner_chain.invoke({
-        "query": query
+        "query": query,
+        "context_summary": context_summary
     })
 
     content = response.content
 
     try:
         parsed_response = json.loads(content)
-
         return parsed_response
 
-    except Exception as e:
+    except Exception as e:      
         return {
             "error": "Invalid JSON response",
             "raw_output": content
