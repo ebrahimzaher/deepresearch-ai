522e4b74337db77c75fdca49e6e22c16751bc103
Ebrahim Zaher <ebrahimzaherr0@gmail.com>
2026-06-16 19:30:56 +0300

    refactor: init by adding revision agent and prompt

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,5 +1,6 @@
 from .planner import planner_agent
 from .researcher import researcher_agent
 from .writer import writer_agent
+from .revision import revision_agent
 from .critic import critic_agent
 from .summarizer import summarizer_agent
